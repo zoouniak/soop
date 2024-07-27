333a46a9b03196998212cdba8456d8a04c3d05b7
jueun oh <jueun1025@naver.com>
2024-07-27 20:02:21 +0900

    first commit

---
+++ soop/src/main/java/com/example/soop/SoopApplication.java
@@ -0,0 +1,13 @@
+package com.example.soop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoopApplication.class, args);
+    }
+
+}
---
+++ soop/src/test/java/com/example/soop/SoopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.soop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
