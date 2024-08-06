d21432ed541078189043859de1d17f70bea1e46d
jueun oh <jueun1025@naver.com>
2024-08-06 19:30:56 +0900

    feat : builder 추가

--- soop/src/main/java/com/example/soop/product/domain/Product.java
+++ soop/src/main/java/com/example/soop/product/domain/Product.java
@@ -1,7 +1,10 @@
 package com.example.soop.product.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,7 @@ import static jakarta.persistence.CascadeType.REMOVE;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +33,13 @@ public class Product {
 
     @OneToMany(mappedBy = "product", cascade = REMOVE, orphanRemoval = true)
     private List<ProductImage> images;
+
+    @Builder
+    public Product(String name, String summary, String description, int price, List<ProductImage> images) {
+        this.name = name;
+        this.summary = summary;
+        this.description = description;
+        this.price = price;
+        this.images = images;
+    }
 }
